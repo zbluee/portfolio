e1b42a56abc15635f06bdc7a80130eb56ee5ab71
zbluee <amanueltemesgen9@gmail.com>
2022-11-10 04:02:05 -0500

    starter file

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      message : 'empty'
+    }
+  }
+  componentDidMount(){
+    fetch('http://localhost:3000/test').then(response => response.json()).then(msg => {
+    console.log(msg);  
+    this.setState({message : msg})
+    }).catch(err => console.log(err))
+  }
+  render(){
+    return(
+      <div className='App'>
+        <h1>{this.state.message}</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
