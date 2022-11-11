bd0df8a19eeca20f19a94ba5f65d1a59d5843bee
zbluee <amanueltemesgen9@gmail.com>
2022-11-11 05:39:40 -0500

    adding fadeIn, slide animation and typewriter effect on home content

--- client/src/components/home-content/home-content.component.jsx
+++ client/src/components/home-content/home-content.component.jsx
@@ -2,19 +2,34 @@ import React from "react";
 import "./home-content.styles.css";
 import { ReactComponent as Hand } from "../../assets/hand.svg";
 import { ReactComponent as Send } from "../../assets/send.svg";
+import Typewriter from "typewriter-effect";
 
 const HomeContent = () => (
   <div className="content">
     <h1 className="title">
-      Amanuel F. <a href="#contact"><Hand /> </a>
+      Amanuel F.{" "}
+      <a href="#contact">
+        <Hand />{" "}
+      </a>
     </h1>
-    <h3 className="subtitle">Web Developer</h3>
+    <h3 className="subtitle">
+      Web
+      <span>
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+            strings: [" Developer.", " Scrapper."],
+          }}
+        />
+      </span>
+    </h3>
     <p className="description">
       I'm web developer and web scrapper who is highly interested on ml and
       cyber security.
     </p>
     <a href="#contact" className="button button-flex">
-      Contact Me <Send  />
+      Contact Me <Send />
     </a>
   </div>
 );
