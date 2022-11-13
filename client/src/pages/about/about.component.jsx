17958618860471d518e65cc3016a8492e1e2fd58
zbluee <amanueltemesgen9@gmail.com>
2022-11-13 09:09:26 -0500

    remove resume file from assect and put it on google drive and add it's link to get resume button in about page

--- client/src/pages/about/about.component.jsx
+++ client/src/pages/about/about.component.jsx
@@ -3,7 +3,6 @@ import "./about.styles.css";
 import ProfileImg from "../../assets/profile.jpg";
 import AboutContent from "../../components/about-content/about-content.component";
 import { ReactComponent as FileDownload } from "../../assets/file-download.svg";
-import Resume from "../../assets/Resume.pdf";
 
 const About = () => (
   <section className="about section" id="about">
@@ -14,11 +13,15 @@ const About = () => (
       <div className="about-content">
         <AboutContent />
         <p className="about-descriprtion">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id
-          interdum metus. Nulla facilisis quis sem eu auctor. Nam pretium ante
-          nec felis maximus congue. Ut vel ligula lectus.
+          Computer Science student at Addis Ababa University | Web Scraper | assist in accessing good data
+          extraction, ingesting, and handling while saving valuable time
         </p>
-        <a download href={Resume} className="button button-flex">
+        <a
+          href="https://drive.google.com/file/d/1DEf_eLGFbrtkQNotQAm02IIEmJ3ZYqz3/view?usp=share_link"
+          className="button button-flex"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Get Resume <FileDownload />
         </a>
       </div>
