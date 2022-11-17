3fe5b3e8dfe4cc987332825a25fc0825a337f05d
zbluee <amanueltemesgen9@gmail.com>
2022-11-17 08:31:40 -0500

    adding comments router for /api/v1/comments route

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors"
+import { commentsRoute } from "./routes/comments.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json())
 app.use(cors())
 
-app.get("/test", (req, res) => {
-  res.json("success");
-});
+app.use("/api/v1/comments", commentsRoute)
+
 
 app.listen(port, () => console.log(`server is listening on port ${port}`));
