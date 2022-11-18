52b185a1fc2b5143e4c81e189b14c8042e6e8fc5
zbluee <amanueltemesgen9@gmail.com>
2022-11-18 04:10:59 -0500

    adding error handler and not found middleware in api route

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import { commentsRoute } from "./routes/comments.js";
+import { notFoundMiddleware } from "./middleware/not-found.js";
+import { errorHandlerMiddleware } from "./middleware/error-handler.js";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use("/api/v1/comments", commentsRoute)
-
+app.use("/api/v1/comments", commentsRoute);
+app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
 
 app.listen(port, () => console.log(`server is listening on port ${port}`));
