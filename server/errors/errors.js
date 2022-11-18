8cb50940e7ac45f72660f0900df5722bd4345bdb
zbluee <amanueltemesgen9@gmail.com>
2022-11-18 04:02:06 -0500

    create custom api errors

---
+++ server/errors/errors.js
@@ -0,0 +1,28 @@
+import { StatusCodes } from "http-status-codes";
+
+class CustomApiError extends Error {
+  constructor(message) {
+    super(message);
+  }
+}
+
+class BadRequestError extends CustomApiError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+class UnauthenticatedError extends CustomApiError {
+  constructor(message) {
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+class NotFoundError extends CustomApiError {
+  constructor(message) {
+    this.statusCode = StatusCodes.NOT_FOUND;
+  }
+}
+
+export { CustomApiError, BadRequestError, UnauthenticatedError, NotFoundError };
