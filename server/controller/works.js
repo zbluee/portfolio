0535adf0bd0aa10b7c0a2e78352c73c6f505db9e
zbluee <amanueltemesgen9@gmail.com>
2022-11-23 05:05:19 -0500

    done with work controller and add authenticate routes with auth middleware in works route

--- server/controller/works.js
+++ server/controller/works.js
@@ -1,23 +1,62 @@
 import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/errors.js";
+import Work from "../models/work.js";
 
 const getWorks = async (req, res) => {
-  res.status(StatusCodes.OK).json({ success: true, data: "get all works" });
+  const works = await Work.find({});
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, count: works.length, works });
 };
 
 const createWork = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ success: true, msg: "create work" });
+  const work = await Work.create({ ...req.body, createdBy: req.user.id });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ success: true, msg: "Successfully Created" });
 };
 
 const getWork = async (req, res) => {
-  res.status(StatusCodes.OK).json({ success: true, data: "get a works" });
+  const {
+    params: { id: workId },
+    user: { id: userId },
+  } = req;
+  const work = await Work.findOne({ _id: workId, createdBy: userId });
+  if (!work) throw new BadRequestError(`No work with ${workId} found`);
+  res.status(StatusCodes.OK).json({ success: true, work });
 };
 
 const updateWork = async (req, res) => {
-  res.status(StatusCodes.OK).json({ success: true, data: "update a works" });
+  const {
+    params: { id: workId },
+    user: { id: userId },
+    body: { title, imageUrl, addressLink },
+  } = req;
+  if (!title || !imageUrl || !addressLink)
+    throw new BadRequestError(
+      "please provide title, imageUrl and address link"
+    );
+  const work = await Work.findOneAndUpdate(
+    { _id: workId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!work) throw new BadRequestError(`No work with ${workId} found`);
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, work, msg: "Successfully updated" });
 };
 
 const deleteWork = async (req, res) => {
-  res.status(StatusCodes.OK).json({ success: true, msg: "delete a works" });
+  const {
+    params: { id: workId },
+    user: { id: userId },
+  } = req;
+  const work = await Work.findByIdAndDelete({ _id: workId, createdBy: userId });
+  if (!work) throw new BadRequestError(`No work with ${workId} found`);
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, msg: "Successfully deleted" });
 };
 
 export { createWork, deleteWork, getWork, getWorks, updateWork };
--- server/routes/works.js
+++ server/routes/works.js
@@ -6,10 +6,11 @@ import {
   getWorks,
   updateWork,
 } from "../controller/works.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.route("/").get(getWorks).post(createWork);
-router.route("/:id").get(getWork).patch(updateWork).delete(deleteWork);
+router.route("/").get(getWorks).post( authMiddleware, createWork);
+router.route("/:id").get( authMiddleware, getWork).patch( authMiddleware, updateWork).delete( authMiddleware, deleteWork);
 
 export {router as worksRoute}
