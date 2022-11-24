630974ad8408cc14a430b53518c975300c31d32c
zbluee <amanueltemesgen9@gmail.com>
2022-11-24 05:53:48 -0500

    Set up security packages and include them as middleware

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 import {} from "dotenv/config";
 import "express-async-errors";
+
+//security packages
+import cors from "cors";
+import helmet from "helmet";
+import xss from "xss-clean"
+
 import express from "express";
 import { connectDB } from "./db/connection.js";
 import { authRoute } from "./routes/auth.js";
 import { commentsRoute } from "./routes/comments.js";
 import { notFoundMiddleware } from "./middleware/not-found.js";
 import { errorHandlerMiddleware } from "./middleware/error-handler.js";
-
-// external security packages
-import cors from "cors";
 import { worksRoute } from "./routes/works.js";
 import { testimoniesRoute } from "./routes/testimonies.js";
 import { skillsRoute } from "./routes/skills.js";
@@ -18,9 +21,13 @@ import { aboutRoute } from "./routes/about.js";
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cors());
+app.use(helmet())
+app.use(xss())
 
+//routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/comments", commentsRoute);
 app.use("/api/v1/works", worksRoute);
