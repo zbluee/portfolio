24d75c2912384b1825e72947bc950ba41d5323eb
zbluee <amanueltemesgen9@gmail.com>
2022-11-24 05:50:28 -0500

    handling mongoose validation, duplicate and cast errors

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -5,6 +5,25 @@ export const errorHandlerMiddleware = async (err, req, res, next) => {
     message: err.message || "Something went wrong. Please try again later.",
   };
   // db errors will handled here
+  // CastError
+  if (err.name === "CastError") {
+    customError.message = `No item with id : ${err.value} found`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  //Validation Error
+  if (err.name === "ValidationError") {
+    customError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  //Duplicate Error
+  if ((err.code = 11000)) {
+    customError.message = `Please choose another value, duplicate value found : ${Object.keys(
+      err.keyValue
+    )} `;
+    customError.statusCode = StatusCodes.BAD_GATEWAY;
+  }
 
   return res
     .status(customError.statusCode)
