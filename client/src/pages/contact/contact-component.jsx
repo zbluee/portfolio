ddc98b565f6cf11145933fbc7f7fa819b8ca7575
zbluee <amanueltemesgen9@gmail.com>
2022-11-27 11:53:24 -0500

    testing the deployed api using production url

--- client/src/pages/contact/contact-component.jsx
+++ client/src/pages/contact/contact-component.jsx
@@ -36,9 +36,11 @@ class Contact extends React.Component {
 
     if (!Object.keys(errors).length) {
       this.setState({ loading: true });
-      const url = "http://localhost:3001/api/v1/comments";
+      // const url = "http://localhost:3001/api/v1/comments";
+      const productionUrl = "https://zbluee-portfolio-api.herokuapp.com/api/v1/comments"
       try {
-        const res = await axios.post(url, { name, email, message });
+        const res = await axios.post(productionUrl, { name, email, message });
+        console.log(res);
         this.setState({
           successMsg: res.data.msg,
           name: "",
