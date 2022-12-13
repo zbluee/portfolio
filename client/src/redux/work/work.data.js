41a75e50a6d1b386250d1663469c03e41eadb648
zbluee <amanueltemesgen9@gmail.com>
2022-12-13 12:22:14 -0500

    update addresslink for work template

--- client/src/redux/work/work.data.js
+++ client/src/redux/work/work.data.js
@@ -9,13 +9,13 @@ const WORK_DATA = [
     id: 1,
     title: "API",
     imageUrl: "https://i.ibb.co/1v73R3T/jobs-api.png",
-    addressLink: "https://jobs-api-job.herokuapp.com/api-docs/",
+    addressLink: "https://jobs-api-ecxk.onrender.com/api-docs/",
   },
   {
     id: 2,
     title: "Portfolio API",
     imageUrl: "https://i.ibb.co/KVTp7v5/portfolio-api.png",
-    addressLink: "https://zbluee-portfolio-api.herokuapp.com/api/v1/docs/",
+    addressLink: "https://zbluee-portfolio-api.onrender.com/api/v1/docs/",
   },
   
 ];
